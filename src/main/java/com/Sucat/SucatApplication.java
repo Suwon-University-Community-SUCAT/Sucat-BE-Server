d93d21f60456690c21d19040e801ea29043c9882
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-05-13 01:48:52 +0900

    Project Settings

---
+++ src/main/java/com/Sucat/SucatApplication.java
@@ -0,0 +1,13 @@
+package com.Sucat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SucatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SucatApplication.class, args);
+	}
+
+}
