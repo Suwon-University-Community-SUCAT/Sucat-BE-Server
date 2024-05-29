c14f47125d0ced58473254f7e38f161441fa084f
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-05-29 11:27:01 +0900

    SwaggerConfig 경로 에러 해결

--- src/main/java/com/Sucat/global/util/SwaggerConstants.java
+++ src/main/java/com/Sucat/global/util/SwaggerConstants.java
@@ -7,8 +7,8 @@ public class SwaggerConstants {
     public static final String[] SWAGGER_APPOINTED_PATHS = {
             "/**"
     };
-    public static final String DEFINITION_TITLE = "Vacation Projdct API 명세서";
-    public static final String DEFINITION_DESCRIPTION = "\uD83D\uDE80 Realtime Congestion Based Location Recommendation Service - SSL Server의 API 명세서입니다.";
+    public static final String DEFINITION_TITLE = "Suwon University Communication Project API 명세서";
+    public static final String DEFINITION_DESCRIPTION = "\uD83D\uDE80 Realtime Congestion Based Location Recommendation Service - Sucat Server의 API 명세서입니다.";
     public static final String DEFINITION_VERSION = "v1";
 
     public static final String SECURITY_SCHEME_NAME = "bearer-key";
@@ -22,9 +22,6 @@ public class SwaggerConstants {
     public static final String PAGING = "pageable";
     public static final String PAGING_DESCRIPTION = "";
 
-    public static final String SORT_KEY = "sortKey";
-    public static final String SORT_KEY_DESCRIPTION = "최신순(기본값): createdAt, 인기순: popular, 고가순: highPrice, 저가순: lowestPrice";
-
     /**
      * Home
      */
@@ -32,6 +29,5 @@ public class SwaggerConstants {
     public static final String TAG_HOME_DESCRIPTION = "Home API";
     public static final String HOME_SUMMARY = "메인 페이지";
     public static final String HOME_DESCRIPTION = "메인 페이지 정보를 가져옵니다.";
-
 
 }
