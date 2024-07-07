cecb3e659461ec9ea04c9f6a881e121d51c5c595
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-07-07 15:17:52 +0900

    feat: default Admin Account Create

--- src/main/java/com/Sucat/domain/user/repository/UserRepository.java
+++ src/main/java/com/Sucat/domain/user/repository/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByNickname(String nickname);
 //    Optional<User> findByRefreshToken(String refreshToken);
+
+    boolean existsByEmail(String email);
 }
