b96133d8edd2df4d73d25327831508cab5ccc470
Ji Hyeon Kim <131633330+Jihyeon02@users.noreply.github.com>
2024-07-11 16:46:33 +0900

    refactor: test

--- src/main/java/com/Sucat/HomeController.java
+++ src/main/java/com/Sucat/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/home")
     public String home() {
-        return "hi";
+        return "test";
     }
 
     @GetMapping("/")
