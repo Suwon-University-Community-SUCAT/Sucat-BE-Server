9d1127a925a38af0571783cf25b8e9bf7a878bf7
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-07-17 19:33:24 +0900

    refactor: Web Socket을 Stomp로 사용하기 위해 config 설정 변경, SockJs 설정을 해서 Web Socket을 지원하지 않는 브라우저에서 Web Socket 요청을 수행하도록 설정

--- src/main/java/com/Sucat/global/config/WebSocketConfig.java
+++ src/main/java/com/Sucat/global/config/WebSocketConfig.java
@@ -1,24 +1,36 @@
 package com.Sucat.global.config;
 
+import com.Sucat.global.security.handler.JwtHandshakeInterceptor;
 import com.Sucat.global.websocket.handler.WebSockChatHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 @RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final WebSockChatHandler webSockChatHandler;
-//    private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
+    private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
+
+    // sockJS Fallback을 이용해 노출할 endpoint 설정
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // WebSocket이 handshake를 하기 위해 연결하는 endpoint
+        registry.addEndpoint("/ws")
+//                .addInterceptors(jwtHandshakeInterceptor)
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); // Web Socket을 지원하지 않는 브라우저에서 HTTP의 Polling과 같은 방식으로 WebSocket의 요청을 수행하도록 돕는다.
+    }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSockChatHandler, "/ws/chat")
-//                .addInterceptors(jwtHandshakeInterceptor)  // JWT 인증 인터셉터 추가
-                .setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // 서버 -> 클라이언트로 발행하는ㅁ ㅔ시지에 대한 endpoint 설정 : 구독
+        config.enableSimpleBroker("/sub"); // 메시지 브로커의 Prefix를 등록, 클라이언트는 토픽을 구독할 시 /sub 경로로 요청해야 함
+
+        // 클라이언트 -> 서버로 발행하는 메시지에 대한 endpoint 설정 : 구독에 대한 메시지
+        config.setApplicationDestinationPrefixes("/pub");
     }
 }
