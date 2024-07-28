94612df4efa2cfc6dfa9ba0423da529dcd60303f
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-07-28 13:59:44 +0900

    feat: 친구 추가 요청, 친구 추가 요청에 알림 추가

--- src/main/java/com/Sucat/domain/notify/model/NotifyType.java
+++ src/main/java/com/Sucat/domain/notify/model/NotifyType.java
@@ -1,5 +1,11 @@
 package com.Sucat.domain.notify.model;
 
 public enum NotifyType {
-    CHAT, POST, REVIEW
+    HOT_POST,            // 핫게시물
+    POST_COMMENT,        // 게시물 댓글
+    COMMENT_REPLY,       // 게시글 댓글의 대댓글
+    FRIEND_REQUEST,      // 친구 추가 요청
+    FRIEND_ACCEPTED,     // 친구 추가 수락
+    CHAT_MESSAGE,        // 채팅
+    NOTICE               // 공지사항
 }
