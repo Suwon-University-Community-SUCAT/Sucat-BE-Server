66046086bbdcdfcd1a6660c70711f3692c573155
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-08-06 15:29:07 +0900

    test: test에서는 local yml 사용하도록 변경

--- src/test/java/com/Sucat/SucatApplicationTests.java
+++ src/test/java/com/Sucat/SucatApplicationTests.java
@@ -2,8 +2,10 @@ package com.Sucat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class SucatApplicationTests {
 
 	@Test
