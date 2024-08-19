0604ce450a29208aa0e2fce5b7c9b9e2016ac507
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-08-19 21:17:11 +0900

    refactor: 필요하지 않은 연관관계 메서드 삭제

--- src/main/java/com/Sucat/domain/scrap/service/ScrapService.java
+++ src/main/java/com/Sucat/domain/scrap/service/ScrapService.java
@@ -35,8 +35,6 @@ public class ScrapService {
             // 이미 스크랩한 경우: 스크랩 취소 (삭제)
             log.info("식별자(boardId): {}, 이미 스크랩한 게시물 -> 스크랩 삭제", boardId);
             scrapRepository.delete(existingScrap);
-            user.removeScrap(existingScrap);
-            board.removeScrap(existingScrap);
         } else {
             // 스크랩하지 않은 경우: 스크랩 추가
             log.info("식별자(boardId): {}, 게시글을 스크랩합니다.", boardId);
