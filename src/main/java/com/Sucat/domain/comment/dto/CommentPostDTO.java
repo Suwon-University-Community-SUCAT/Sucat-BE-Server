469dcb8ca7495c6b71bfb31bb9c173b4916e1dab
JeongAreum <jungarum0607@gmail.com>
2024-08-19 20:29:54 +0900

    댓글 작성 개발

--- src/main/java/com/Sucat/domain/comment/dto/CommentPostDTO.java
+++ src/main/java/com/Sucat/domain/comment/dto/CommentPostDTO.java
@@ -1,19 +1,25 @@
 package com.Sucat.domain.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class CommentPostDTO {
-    private String name;
     private String commentContent;
-    private String minute;
-    private int likeCount;
-    private int commentCount;
-    private int scrapCount;
-    
-    public CommentPostDTO(String name, String commentContent, String minute, int likeCount, int commentCount, int scrapCount, String imageUrl) {
-        this.name = name;
+
+    @JsonCreator
+    public CommentPostDTO(@JsonProperty("commentContent") String commentContent) {
+       this.commentContent = commentContent;
+    }
+
+    public String getCommentContent() {
+        return commentContent;
+    }
+
+    public void setCommentContent(String commentContent) {
         this.commentContent = commentContent;
-        this.minute = minute;
-        this.likeCount = likeCount;
-        this.commentCount = commentCount;
-        this.scrapCount = scrapCount;
     }
 }
