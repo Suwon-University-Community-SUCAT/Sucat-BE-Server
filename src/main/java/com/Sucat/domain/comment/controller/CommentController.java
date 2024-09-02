1972512e1107e2fa892e63c7d24735dae0be39b1
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-09-02 23:09:03 +0900

    refactor: Comment 관련 코드를 다시 리팩토링, 비즈니스 로직이 Service에 집중되도록

--- src/main/java/com/Sucat/domain/comment/controller/CommentController.java
+++ src/main/java/com/Sucat/domain/comment/controller/CommentController.java
@@ -1,12 +1,7 @@
 package com.Sucat.domain.comment.controller;
 
-import com.Sucat.domain.board.model.Board;
-import com.Sucat.domain.board.service.BoardService;
 import com.Sucat.domain.comment.service.CommentService;
-import com.Sucat.domain.notify.model.NotifyType;
-import com.Sucat.domain.notify.service.NotifyService;
 import com.Sucat.domain.user.model.User;
-import com.Sucat.domain.user.service.UserService;
 import com.Sucat.global.annotation.CurrentUser;
 import com.Sucat.global.common.code.SuccessCode;
 import com.Sucat.global.common.response.ApiResponse;
@@ -27,24 +22,15 @@ import static com.Sucat.domain.comment.dto.CommentDto.CommentPostRequest;
 public class CommentController {
 
     private final CommentService commentService;
-    private final BoardService boardService;
-    private final UserService userService;
-    private final NotifyService notifyService;
 
     /* 댓글 생성 */
     @PostMapping("/{boardId}")
     public ResponseEntity<ApiResponse<Object>> write(
             @PathVariable Long boardId,
             @CurrentUser User user,
-            @RequestBody CommentPostRequest commentPostDTO)
+            @RequestBody CommentPostRequest commentPostRequest)
     {
-        Board board = boardService.findBoardById(boardId);
-        String content = commentPostDTO.content();
-        commentService.write(board, user, content);
-
-        notifyService.send(board.getUser(), NotifyType.POST_COMMENT, "새로운 댓글이 달렸습니다: " + content, "/api/v1/boards/"+boardId);
-        log.info("게시물 작성자에게 알림이 전송됩니다. 게시글 식별자: {}", boardId);
-
+        commentService.write(boardId, user, commentPostRequest);
         return ApiResponse.onSuccess(SuccessCode._OK);
     }
 
@@ -55,7 +41,6 @@ public class CommentController {
             @CurrentUser User user
     ) {
         commentService.delete(commentId, user);
-
         return ApiResponse.onSuccess(SuccessCode._OK);
     }
 
@@ -63,7 +48,6 @@ public class CommentController {
     @GetMapping("/my")
     public ResponseEntity<ApiResponse<Object>> myComment(@CurrentUser User user) {
         List<MyBoardResponse> myBoardResponses = commentService.myComment(user);
-
         return ApiResponse.onSuccess(SuccessCode._OK, myBoardResponses);
     }
 }
--- src/main/java/com/Sucat/domain/comment/service/CommentService.java
+++ src/main/java/com/Sucat/domain/comment/service/CommentService.java
@@ -2,9 +2,13 @@ package com.Sucat.domain.comment.service;
 
 import com.Sucat.domain.board.exception.BoardException;
 import com.Sucat.domain.board.model.Board;
+import com.Sucat.domain.board.service.BoardService;
 import com.Sucat.domain.comment.domain.Comment;
+import com.Sucat.domain.comment.dto.CommentDto;
 import com.Sucat.domain.comment.exception.CommentException;
 import com.Sucat.domain.comment.repository.CommentRepository;
+import com.Sucat.domain.notify.model.NotifyType;
+import com.Sucat.domain.notify.service.NotifyService;
 import com.Sucat.domain.user.model.User;
 import com.Sucat.global.common.code.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -22,10 +26,15 @@ import static com.Sucat.domain.board.dto.BoardDto.MyBoardResponse;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final BoardService boardService;
+    private final NotifyService notifyService;
 
     /* 댓글 작성 메서드 */
     @Transactional
-    public void write(Board board, User user, String content) {
+    public void write(Long boardId, User user, CommentDto.CommentPostRequest request) {
+        Board board = boardService.findBoardById(boardId);
+        String content = request.content();
+
         boolean checkWriter = board.getUser().equals(user);
 
         Comment comment = Comment.builder()
@@ -34,9 +43,14 @@ public class CommentService {
                 .build();
 
         commentRepository.save(comment);
-        log.info("식별자: {}, 댓글 작성", comment.getId());
         board.addComment(comment);
         user.addComment(comment);
+
+        log.info("식별자: {}, 댓글 작성", comment.getId());
+
+        // 알림 서비스 호출
+        notifyService.send(board.getUser(), NotifyType.POST_COMMENT, "새로운 댓글이 달렸습니다: " + content, "/api/v1/boards/"+boardId);
+        log.info("게시물 작성자에게 알림이 전송됩니다. 게시글 식별자: {}", boardId);
     }
 
     /* 댓글 삭제 메서드 */
@@ -47,6 +61,7 @@ public class CommentService {
 
         comment.getBoard().removeComment(comment);
         comment.getUser().removeComment(comment);
+
         commentRepository.deleteById(commentId);
         log.info("식별자: {}, 댓글 삭제 완료", commentId);
     }
