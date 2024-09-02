a94f6a85a2cbc2ec838ed0ea38c4c5372cf563a1
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-09-02 21:35:35 +0900

    refactor: Notification 관련 코드를 책임 분리, 단일 책임 원칙에 따라 리팩토링

--- src/main/java/com/Sucat/domain/notification/controller/NotificationController.java
+++ src/main/java/com/Sucat/domain/notification/controller/NotificationController.java
@@ -1,25 +1,20 @@
 package com.Sucat.domain.notification.controller;
 
-import com.Sucat.domain.notification.dto.NotificationDto;
 import com.Sucat.domain.notification.service.NotificationService;
 import com.Sucat.global.common.code.SuccessCode;
 import com.Sucat.global.common.response.ApiResponse;
-import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 import static com.Sucat.domain.notification.dto.NotificationDto.*;
-import static com.Sucat.domain.notification.dto.NotificationDto.CreateNotificationRequest;
-import static com.Sucat.domain.notification.dto.NotificationDto.SystemListWithSizeResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,9 +24,9 @@ public class NotificationController {
     /* 공지사항 작성 */
     @PostMapping("/admin/notification")
     public ResponseEntity<ApiResponse<Object>> createNotification(
-            @RequestPart(name = "notificationRequest") @Valid CreateNotificationRequest createNotificationRequest,
-            @RequestPart(name = "images", required = false) List<MultipartFile> images) throws IOException {
-        notificationService.create(createNotificationRequest.toEntity(), images);
+            @RequestPart(name = "notificationRequest") @Valid CreateNotificationRequest request,
+            @RequestPart(name = "images", required = false) List<MultipartFile> images) {
+        notificationService.create(request.toEntity(), images);
 
         return ApiResponse.onSuccess(SuccessCode._OK);
     }
--- src/main/java/com/Sucat/domain/notification/service/NotificationService.java
+++ src/main/java/com/Sucat/domain/notification/service/NotificationService.java
@@ -7,7 +7,6 @@ import com.Sucat.domain.notification.model.Notification;
 import com.Sucat.domain.notification.repository.NotificationQueryRepository;
 import com.Sucat.domain.notification.repository.NotificationRepository;
 import com.Sucat.global.common.code.ErrorCode;
-import com.Sucat.global.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,38 +15,33 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 import static com.Sucat.domain.notification.dto.NotificationDto.*;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final NotificationQueryRepository notificationQueryRepository;
     private final ImageService imageService;
-    private final JwtUtil jwtUtil;
 
-    public Notification getNotification(Long id) {
-        return notificationRepository.findById(id)
-                .orElseThrow(() -> new NotificationException(ErrorCode.NOTIFICATION_NOT_FOUND));
-    }
-
+    /* 공지사항 작성 메서드 */
     @Transactional
-    public void create(Notification notification, List<MultipartFile> images) throws IOException {
-        notificationRepository.save(notification);
-
+    public void create(Notification notification, List<MultipartFile> images) {
         if (images != null && !images.isEmpty()) {
             imageService.storeFiles(images).forEach(imageName -> {
                 Image image = Image.ofNotification(notification, imageName);
                 notification.addNotificationImage(image);
             });
         }
+        notificationRepository.save(notification);
     }
 
+    /* 공지사항 목록 조회 메서드 */
     public SystemListWithSizeResponse getSystemList(Pageable pageable) {
-        PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(Sort.Direction.DESC, "createdAt"));
+        Pageable pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(Sort.Direction.DESC, "createdAt"));
         List<SystemListResponse> systemList = notificationRepository.findAll(pageRequest).stream()
                 .map(SystemListResponse::of)
                 .toList();
@@ -55,8 +49,15 @@ public class NotificationService {
         return SystemListWithSizeResponse.of(systemList, listSize);
     }
 
+    /* 공지사항 상세 조회 메서드 */
     public NotificationDetailResponse getNotificationDetail(Long notificationId) {
         Notification notification = notificationQueryRepository.findNotificationById(notificationId);
         return NotificationDetailResponse.of(notification);
+    }
+
+    /* Using Method */
+    public Notification findById(Long id) {
+        return notificationRepository.findById(id)
+                .orElseThrow(() -> new NotificationException(ErrorCode.NOTIFICATION_NOT_FOUND));
     }
 }
