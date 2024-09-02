c59645bc5232569ca4fd9cb89d27d3c310d97065
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-09-02 23:02:30 +0900

    refactor: ChatMessage 관련 코드를 다시 리팩토링, 비즈니스 로직이 Service에 집중되도록

--- src/main/java/com/Sucat/domain/chatmessage/controller/ChatMessageController.java
+++ src/main/java/com/Sucat/domain/chatmessage/controller/ChatMessageController.java
@@ -4,20 +4,12 @@ import com.Sucat.domain.chatmessage.dto.ChatMessageDto;
 import com.Sucat.domain.chatmessage.dto.ChatMessageResponse;
 import com.Sucat.domain.chatmessage.model.ChatMessage;
 import com.Sucat.domain.chatmessage.service.ChatMessageService;
-import com.Sucat.domain.chatroom.model.ChatRoom;
-import com.Sucat.domain.chatroom.service.ChatRoomService;
-import com.Sucat.domain.user.model.User;
-import com.Sucat.domain.user.service.UserService;
 import com.Sucat.global.common.code.SuccessCode;
 import com.Sucat.global.common.response.ApiResponse;
 import com.Sucat.global.common.response.PageInfo;
-import com.Sucat.global.redis.PublishMessage;
-import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -26,7 +18,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -35,41 +26,23 @@ import java.util.List;
 public class ChatMessageController {
 
     private final ChatMessageService chatMessageService;
-    private final UserService userService;
-    private final ChatRoomService chatRoomService;
-    private final ChannelTopic topic;
 
-    @Resource(name = "chatRedisTemplate")
-    private final RedisTemplate<String, Object> redisTemplate;
-
-    @MessageMapping("/chats/messages/{roomId}") // '/pub/chats/messages/{room-id}' 경로로 보낸 메시지를 처리
+    // TODO 보낸 사용자 정보를 senderId로 받는 것이 아닌, 토큰 정보를 조회에서 하도록 변경
+    @MessageMapping("/chats/messages/{roomId}")
     public void message(@DestinationVariable("roomId") String roomId, ChatMessageDto chatMessageDto) {
         Long senderId = chatMessageDto.getSenderId();
         String content = chatMessageDto.getContent();
 
-        PublishMessage publishMessage =
-                new PublishMessage(roomId, senderId, content, LocalDateTime.now());
-        log.info("publishMessage: {}", publishMessage.getContent());
-
-        //Redis를 통해 메시지 전송
-        redisTemplate.convertAndSend(topic.getTopic(), publishMessage);
-
-        User user = userService.findById(senderId);
-        ChatRoom chatRoom = chatRoomService.findByRoomId(roomId);
-
-        chatMessageService.saveMessage(content, user, chatRoom);
+        chatMessageService.handleMessage(roomId, senderId, content);
     }
 
-
     // 채팅방 메시지 목록 가져오기
-    @GetMapping("/api/v1/chats/messages/{room-id}")
-    public ResponseEntity<ApiResponse<Object>> getMessages(@PathVariable("room-id") String roomId,
+    @GetMapping("/api/v1/chats/messages/{roomId}")
+    public ResponseEntity<ApiResponse<Object>> getMessages(@PathVariable("roomId") String roomId,
                                                            @RequestParam(defaultValue = "1") int page,
                                                            @RequestParam(defaultValue = "10") int size) {
-        ChatRoom chatRoom = chatRoomService.findByRoomId(roomId);
-
         Page<ChatMessage> messages =
-                chatMessageService.getChatRoomMessages(chatRoom, page, size);
+                chatMessageService.getChatRoomMessages(roomId, page, size);
         PageInfo pageInfo = new PageInfo(page, size, (int)messages.getTotalElements(), messages.getTotalPages());
 
         List<ChatMessageResponse.ChatRoomMessageResponse> chatRoomMessageResponses = messages.getContent().stream().map(
--- src/main/java/com/Sucat/domain/chatmessage/service/ChatMessageService.java
+++ src/main/java/com/Sucat/domain/chatmessage/service/ChatMessageService.java
@@ -3,22 +3,49 @@ package com.Sucat.domain.chatmessage.service;
 import com.Sucat.domain.chatmessage.model.ChatMessage;
 import com.Sucat.domain.chatmessage.repository.ChatMessageRepository;
 import com.Sucat.domain.chatroom.model.ChatRoom;
+import com.Sucat.domain.chatroom.service.ChatRoomService;
 import com.Sucat.domain.user.model.User;
+import com.Sucat.domain.user.service.UserService;
+import com.Sucat.global.redis.PublishMessage;
+import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ChatMessageService {
+    private final ChannelTopic topic;
+    @Resource(name = "chatRedisTemplate")
+    private final RedisTemplate<String, Object> redisTemplate;
+
     private final ChatMessageRepository chatMessageRepository;
+    private final ChatRoomService chatRoomService;
+    private final UserService userService;
+
+    /* 메시지 전송 메서드 */
+    @Transactional
+    public void handleMessage(String roomId, Long senderId, String content) {
+        User user = userService.findById(senderId);
+        ChatRoom chatRoom = chatRoomService.findByRoomId(roomId);
+
+        // Redis를 통해 메시지 전송
+        PublishMessage publishMessage = new PublishMessage(roomId, senderId, content, LocalDateTime.now());
+        redisTemplate.convertAndSend(topic.getTopic(), publishMessage);
+
+        saveMessage(content, user, chatRoom);
+    }
 
     /* 메시지 저장 메서드 */
     @Transactional
@@ -35,10 +62,9 @@ public class ChatMessageService {
     }
 
     /* 채팅방 메시지 목록 조회 메서드 */
-    public Page<ChatMessage> getChatRoomMessages(ChatRoom chatRoom, int page, int size) {
-
+    public Page<ChatMessage> getChatRoomMessages(String roomId, int page, int size) {
+        ChatRoom chatRoom = chatRoomService.findByRoomId(roomId);
         Pageable pageable = PageRequest.of(page-1, size, Sort.by("id").descending());
-
         return chatMessageRepository.findByChatRoom(pageable, chatRoom);
     }
 }
