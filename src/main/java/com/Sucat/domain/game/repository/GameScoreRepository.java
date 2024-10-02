b31cdc6e3d0b64e5c0bb9afdf689dbbed64a5e83
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-10-02 14:19:10 +0900

    feat: 랭킹 조회를 위한 query 문을 작성

--- src/main/java/com/Sucat/domain/game/repository/GameScoreRepository.java
+++ src/main/java/com/Sucat/domain/game/repository/GameScoreRepository.java
@@ -1,8 +1,39 @@
 package com.Sucat.domain.game.repository;
 
+import com.Sucat.domain.game.model.GameCategory;
 import com.Sucat.domain.game.model.GameScore;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+import static com.Sucat.domain.game.dto.GameDto.GameRankingResponse;
+import static com.Sucat.domain.game.dto.GameDto.UserRankingResponse;
 
 public interface GameScoreRepository extends JpaRepository<GameScore, Long> {
     GameScore findByUserIdAndGameId(Long userId, Long gameId);
+
+    /* 상위 10명 점수 조회 */
+    @Query(value = "SELECT u.id AS userId, u.nickname, gs.score " +
+            "FROM GameScore gs " +
+            "JOIN gs.user u " +
+            "JOIN gs.game g " +
+            "WHERE g.category = :category " +
+            "ORDER BY gs.score DESC")
+    List<GameRankingResponse> findTop10PlayersByCategory(@Param("category") GameCategory category, Pageable pageable);
+
+    /* 사용자 개인 랭킹, 점수 조회 */
+    @Query(value = "SELECT COALESCE(r.rank, 0) AS rank, COALESCE(r.score, 0) AS score " +
+            "FROM (" +
+            "    SELECT gs.user.id AS userId, gs.score, " +
+            "           ROW_NUMBER() OVER (ORDER BY gs.score DESC) AS rank " +
+            "    FROM GameScore gs " +
+            "    JOIN gs.game g " +
+            "    WHERE g.category = :category " +
+            ") r " +
+            "WHERE r.userId = :userId ")
+    UserRankingResponse findUserRankingByCategory(@Param("userId") Long userId, @Param("category") GameCategory category);
+
 }
