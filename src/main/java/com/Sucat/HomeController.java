d44eece48dd29d61f01b2005286ad7b37710b59c
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2024-11-16 16:06:02 +0900

    feat: Add AWS S3 image upload method

--- src/main/java/com/Sucat/HomeController.java
+++ src/main/java/com/Sucat/HomeController.java
@@ -1,10 +1,21 @@
 package com.Sucat;
 
+import com.Sucat.domain.image.service.S3Uploader;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final S3Uploader s3Uploader;
+
     @GetMapping("/home")
     public String home() {
         return "test";
@@ -19,4 +30,10 @@ public class HomeController {
     public String healthcheck() {
         return "OK";
     }
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("data") MultipartFile multipartFile) throws IOException {
+        return s3Uploader.upload(multipartFile, "image");
+    }
+
 }
