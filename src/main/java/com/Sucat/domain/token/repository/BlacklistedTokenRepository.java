098bc9d568fdb70e2e75945f8ba98dcb18c42791
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-02-26 02:13:46 +0900

    fix: Component 명시가 안되어 있던 문제 해결 - CustomLogoutFilter에서 사용하는 2가지 Repo에서 컴포넌트 명시가 되어 있지 않아 테스트 코드에서 문제 발생 - @Repository 어노테이션을 추가해 해결

--- src/main/java/com/Sucat/domain/token/repository/BlacklistedTokenRepository.java
+++ src/main/java/com/Sucat/domain/token/repository/BlacklistedTokenRepository.java
@@ -3,9 +3,11 @@ package com.Sucat.domain.token.repository;
 import com.Sucat.domain.token.model.BlacklistedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 
+@Repository
 public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, String> {
     boolean existsByToken(String token);
     void deleteByCreatedAt(@Param("date") final LocalDateTime date);
--- src/main/java/com/Sucat/domain/token/repository/TokenRepository.java
+++ src/main/java/com/Sucat/domain/token/repository/TokenRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
     @Query("SELECT u FROM Token u WHERE u.email = :email")
     Optional<Token> findByEmail(String email);
